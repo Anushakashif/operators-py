b8c4b40873cd7c186173b86ac695f48bc5a01522
Anusha Kashif <anushakashiff@gmail.com>
2025-02-25 12:55:07 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+#Arithmetic operator
+#Assigment operator
+#Comparison operator
+#Identity operator 
+#Membership operator
+#Bitwise operator 
+
+#Arithmetic operator
+print("5 + 6 is", 5+6)
+print("5 - 6 is", 5-6)
+print("5 * 6 is", 5*6)
+print("5 / 6 is", 5/6)
+print("15 // 6 is", 15//6)
+print("5 ** 6 is", 5**6)
+print("5 % 6 is", 5%6)
+
+#Assigment operator
+x = 5
+print(x)
+x += 7
+print(x)
+
+y = 5
+print(y)
+y -= 7
+print(y)
+
+#Comparison operator
+i = 8
+print(i == 8)
+
+i = 8
+print(i == 5)
+
+i = 8
+print(i != 5)
+
+i = 8
+print(i <= 5)
+
+i = 8
+print(i >= 5)
+
+#Logical operator
+a = True
+b = False
+print(a and b) # any 1 is false and will print false
+
+a = True
+b = True
+print(a and b)
+
+a = True
+b = False
+print(a or b) # any 1 is true or will print true
+
+a = True
+b = False
+print(a is b)
+
+a = True
+b = False
+print(a is not b)
+
+#Identity operator
+print(5 is not 5)
+
+#Membership operator
+list =[33, 45, 78, 90]
+print(45 in list)
+print(378 in list)
+
+#Bitwise operator (use binary 0,1)
+#0 = 00
+#1 = 01
+#2 = 10
+#3 = 11
+
+print(0 & 2)
+print(0 | 2)
+
